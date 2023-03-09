872856cc819a3381076cec0044f65b4e427677b1
Seunghwa-Han <22000786@handong.ac.kr>
2023-03-09 16:54:17 +0900

    temp

--- Coding Test/README.md
+++ Coding Test/README.md
@@ -11,7 +11,7 @@
 | 9093 | 단어 뒤집기 | istringstream, auto, algorithm: reverse, stack |
 | 9012 | 괄호 | stack |
 | 1874 | 스택수열 | stack |
-| 1406 | 에디터 |  |
+| 1406 | 에디터 | list |
 
 </details>
 
--- Coding Test/baekjoon/b_1406.cpp
+++ Coding Test/baekjoon/b_1406.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <list> 
+#include <functional>
 using namespace std;
 
 /*
@@ -12,11 +14,48 @@ size: 7
 int main(){
     string input, command;  // 문자열은 길이가 N이고, 영어 소문자로만 이루어져 있으며, 길이는 100,000을 넘지 않는다.
     int M; // 1 ≤ M ≤ 500,000
-    int cursor = 0, size;
+    int size;
+    list<char> list;
+
     getline(cin, input);
     size = input.length();
     
-    scanf("%d", &M);
+    scanf("%d\n", &M);
+
+    for(int i=0; i<size; i++){
+        list.push_back(input[i]);
+    }
+
+    for(int i=0; i<M; i++){
+        getline(cin, command);
+
+        if(command == "L"){
+        }
+        else if(command == "D"){
+        }
+        else if(command == "B"){
+        }
+        else{  // P $
+            string ch = command.substr(2);
+        }
+    }
+
+    cout << input <<endl;
+
+    return 0;
+}
+
+/*
+
+시간초과 
+int main(){
+    string input, command;  // 문자열은 길이가 N이고, 영어 소문자로만 이루어져 있으며, 길이는 100,000을 넘지 않는다.
+    int M; // 1 ≤ M ≤ 500,000
+    int cursor, size;
+    getline(cin, input);
+    size = cursor = input.length();
+    
+    scanf("%d\n", &M);
 
     for(int i=0; i<M; i++){
         getline(cin, command);
@@ -30,12 +69,19 @@ int main(){
                 cursor++;
         }
         else if(command == "B"){
-            
+            if(cursor > 0){
+                input.erase(--cursor, 1);
+            }
         }
         else{  // P $
-
+            string ch = command.substr(2);
+            input.insert(cursor++, ch);
+            size = input.length();
         }
     }
 
+    cout << input <<endl;
+
     return 0;
 }
+*/
