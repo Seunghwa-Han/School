97a19674cf34be857764e3df014d379bbf5263cf
Seunghwa-Han <22000786@handong.ac.kr>
2023-03-10 15:23:06 +0900

    temp

---
+++ Coding Test/baekjoon/b_10845.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main(){
+    int N; //1 ≤ N ≤ 10,000
+    string str; 
+
+    scanf("%d\n", &N);
+
+    for(int i=0; i<N; i++){
+        getline(cin, str);
+
+        
+    }
+
+    return 0;
+}
