dd1803337ef7e2ecbc9093c243a19a6db87e8643
SH <22000786@handong.edu>
2023-07-24 16:39:48 +0900

    solve brute force-DFS

--- Coding Test/baekjoon/b_14225.py
+++ Coding Test/baekjoon/b_14225.py
@@ -1,25 +1,33 @@
 N = int(input()) # 1 ≤ N ≤ 20
 S = list(map(int, input().split()))
 
-def dfs(idx, dep, sum_):
-	if dep == length:
-		out.add(sum_)
-		return 
-	
-	for i in range(idx, N):
-		sum_ += S[i]
-		dfs(i+1, dep+1, sum_)
-		sum_ -= S[i]
+# def dfs(idx, dep, sum_):
+# 	if dep == length:
+# 		out.add(sum_)
+# 		return 
+
+# 	for i in range(idx, N):
+# 		sum_ += S[i]
+# 		dfs(i+1, dep+1, sum_)
+# 		sum_ -= S[i]
+
+# out = set()
+# for length in range(1, N+1):
+# 	dfs(0,0,0)
 
 out = set()
-for length in range(1, N+1):
-	dfs(0,0,0)
+def dfs2(idx, sum_):
+	if idx == N:
+		return 
+	sum_ += S[idx]
+	out.add(sum_)
+	dfs2(idx+1, sum_)
+	sum_ -= S[idx]
+	dfs2(idx+1, sum_)
+dfs2(0,0)
 
 res = set()
-for i in range(1, max(out)+1):
+for i in range(1, max(out)+2):
 	res.add(i)
 res -= out 
-if res:
-	print(min(res))
-else:
-	print(max(out)+1)
+print(min(res))
