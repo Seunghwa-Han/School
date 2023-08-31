4372bc08232664fec37b99f03cf1b3d200b9a0c8
Seunghwa-Han <22000786@handong.ac.kr>
2023-08-31 16:56:05 +0900

    solve graph

---
+++ Coding Test/programmers/순위.py
@@ -0,0 +1,26 @@
+def solution(n, results):
+    answer = 0
+    order = [0 for _ in range(n+1)]
+    graphs = [[0]*(n+1) for _ in range(n+1)]
+    for u, v in results:
+        graphs[v][u] = 1
+    
+    for k in range(1, n+1):
+        prev = graphs.copy()
+        for i in range(1, n+1):
+            if i ==k:
+                continue
+            for j in range(1, n+1):
+                if j==k or j==i :
+                    continue 
+                if graphs[i][k] and graphs[k][j] and not graphs[i][j]:
+                        graphs[i][j]=1
+    
+    for i in range(1, n+1):
+        cnt = graphs[i].count(1)
+        for j in range(1, n+1):
+            if graphs[j][i]:
+                cnt+=1
+        if cnt == n-1:
+            answer += 1
+    return answer
