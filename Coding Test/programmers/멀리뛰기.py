d5c30784d2f88414c9cecdbfabd96aefa09f7ac9
SH <22000786@handong.edu>
2023-10-07 20:36:33 +0900

    solve dp

--- Coding Test/programmers/멀리뛰기.py
+++ Coding Test/programmers/멀리뛰기.py
@@ -38,4 +38,21 @@ factorial 사용하려 했는데도 그랬음
 그래서 for문으로 factorial 구했는데도 그랬음 
 -> /을 //로 바꾸니까 통과. 오버플로우 난듯 .. 
 
+근데 이 문제를 dp로 풀 수 있다고 함 ..! 
+n=1 : 1
+n=2 : 2
+n=3 : 3
+n=4 : 5
+n=5 : 8
+f(n) = f(n-1) + f(n-2)
 '''
+def solution(n):
+    f = [0]*(2001)
+    
+    f[1]=1
+    f[2]=2
+    for i in range(3, n+1):
+        f[i] = f[i-1]+f[i-2]
+        
+    ans = f[n]%1234567
+    return ans
