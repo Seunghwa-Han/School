384466ca4da467c8e8d851aed1eb49033bcffc01
Seunghwa-Han <22000786@handong.ac.kr>
2023-10-11 13:19:06 +0900

    solve dfs

--- Coding Test/programmers/여행경로.py
+++ Coding Test/programmers/여행경로.py
@@ -25,7 +25,6 @@ def dfs(fly, d, ans):
     elif not d.get(fly): # 모든 edge를 소비하지 않았는데 갈 곳이 없는 경우 실패 (다시 돌아가야함)
         return False
 
-    flag = False
     for i in range(len(d[fly])-1, -1,-1):
         next = d[fly][i]
         d[fly].remove(next)
@@ -39,11 +38,10 @@ def dfs(fly, d, ans):
             d[fly].append(next)
             cnt += 1
         else:
-            flag = True 
-            break
+            return True 
     # recursion 호출 반환값이 true이면 성공이므로 return true
     # 반환값이 true가 아니라면 잘못된 경로로 온 것이므로 return false 
-    return flag
+    return False
 '''
 https://school.programmers.co.kr/questions/33058
 [예외 test case]
@@ -54,4 +52,57 @@ https://school.programmers.co.kr/questions/33058
 2. dictionary value를 역순으로 정렬시켜 놓고 
     뒤에서부터 반복문돌면서 함수 호출 
     반복문 사용-> 사전순으로 앞선 경로로 가면 모든 edge를 순회못할경우 다른 길로 가야하기 때문에 
+
+    
+그냥 dict 사용하면 key가 없을 때 key error가 뜨니까 defaultdict를 사용하자 ..!
+'''
+
+cnt =0 
+def solution(tickets):
+    global cnt 
+    cnt = len(tickets)
+    d = dict()
+    
+    for a,b in tickets:
+        if not d.get(a):
+            d[a]=[b]
+        else:
+            d[a].append(b)
+        d[a].sort()
+    
+    ans = dfs("ICN", d, ["ICN"])
+    
+    return ans
+
+def dfs(fly, d, ans):
+    global cnt
+    if cnt==0: 
+        return ans
+    elif not d.get(fly):
+        return False
+
+    for i, next in enumerate(d[fly]):
+        d[fly].pop(i)
+        tmp = ans[:]
+        tmp.append(next)
+        cnt -= 1 
+        r = dfs(next, d, tmp)
+        
+
+        d[fly].insert(i, next)
+        cnt += 1
+        if r:
+            return r
+    return False 
+
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/43164/solution_groups?language=python3
+첫번째 코드보고 조금 수정해봤음 
+- 반복문 안에서 return 값이 false일때만 경로 삭제해주지 않아도 됨 -> recursion을 다 돌고 return 되었다는 거는 해당경로를 통해 dfs를 다 한거니까
+- return값이 true일때는 경로집합(ans)를 리턴해주면됨 !! 
+다 돌지 못하는 경우는 없으니까 최종 return값이 false인 경우는 없음 
+
+return false해주는 부분 사실 필요없는데 (return값 없으면 None이 리턴되니까)
+key error때문에 추가해주었음 
+여러모로 defaultdict을 사용하는게 편할듯..!!
 '''
